1645b23cd6e59376c18c38e970fb2c0ed05a55b0
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-03-30 06:54:34 +0300

    Completed lab

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(i => {
+    
+    const words = i.split(' ');
+    
+    const titledWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    
+    return titledWords.join(' ');
+  });
 }
+
